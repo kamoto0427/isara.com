2a64429a526214192e2e4bda07186a2fd875e3f7
kamoto0427 <takahiro.08039@gmail.com>
2021-01-13 10:56:33 +0900

    アコーディオンパネルの準備

--- question.js
+++ question.js
@@ -1,5 +1,6 @@
 $(function() {
-  $(".q-text").click(function() {
-    console.log('hello');
-  });
+  $('.q-answer').css('display', 'none'); //質問の答えの非表示
+  $('.q-first').css('display', 'block'); //最初の質問の答えを初めから表示
+
+  
 });
