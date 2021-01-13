f41440186e17b85bf7b5caf25002298e1eb17bfd
kamoto0427 <takahiro.08039@gmail.com>
2021-01-13 11:28:59 +0900

    アコーディオンの完成

--- question.js
+++ question.js
@@ -2,5 +2,14 @@ $(function() {
   $('.q-answer').css('display', 'none'); //質問の答えの非表示
   $('.q-first').css('display', 'block'); //最初の質問の答えを初めから表示
 
-  
+  $('.q-inner').click(function() {
+    var $answer = $(this).find('.q-answer');
+    if($answer.hasClass('open')) {
+      $answer.removeClass('open');
+      $answer.slideUp();
+    } else {
+      $answer.addClass('open');
+      $answer.slideDown();
+    }
+  })
 });
