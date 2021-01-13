a6a80a9920721e40c0c64e4dde8dec3b4eae0b48
kamoto0427 <takahiro.08039@gmail.com>
2021-01-13 14:05:21 +0900

    アコーディオンのアイコン開閉修正

--- question.js
+++ question.js
@@ -1,15 +1,17 @@
 $(function() {
   $('.q-answer').css('display', 'none'); //質問の答えの非表示
-  $('.q-first').css('display', 'block'); //最初の質問の答えを初めから表示
 
   $('.q-inner').click(function() {
     var $answer = $(this).find('.q-answer');
     if($answer.hasClass('open')) {
       $answer.removeClass('open');
       $answer.slideUp();
+      $(this).find('#qc-upBottom').removeClass().addClass('fas fa-chevron-circle-down');
+
     } else {
       $answer.addClass('open');
       $answer.slideDown();
+      $(this).find('#qc-upBottom').removeClass().addClass('fas fa-chevron-circle-up');
     }
   })
 });
